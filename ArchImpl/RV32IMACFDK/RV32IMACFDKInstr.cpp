50e9b85a147605e3a1e994e074e783ad3ea1c9b3
Philipp van Kempen <philipp.van-kempen@tum.de>
2025-02-26 14:44:46 +0100

    update RV32IMACFDK etiss architecture

---
+++ ArchImpl/RV32IMACFDK/RV32IMACFDKInstr.cpp
@@ -0,0 +1,13 @@
+/**
+ * Generated on Wed, 26 Feb 2025 14:44:45 +0100.
+ *
+ * This file contains the instruction behavior models of the default
+ * instruction set for the RV32IMACFDK core architecture.
+ */
+
+#include "RV32IMACFDKArch.h"
+#include "RV32IMACFDKFuncs.h"
+
+using namespace etiss;
+using namespace etiss::instr;
+
