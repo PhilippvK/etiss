5657a10495ac10d4bac2add094abbc23ecaf3a00
Daniel Schloms <daniel.schloms@gmail.com>
2025-09-01 10:44:53 +0000

    Add zvl64b version of arch

---
+++ ArchImpl/RV32IMACFDV_zvl64b/RV32IMACFDV_zvl64b_riscv_encodingInstr.cpp
@@ -0,0 +1,14 @@
+/**
+ * Generated on Wed, 06 Jul 2022 15:28:06 +0200.
+ *
+ * This file contains the instruction behavior models of the riscv_encoding
+ * instruction set for the RV32IMACFDV_zvl64b core architecture.
+ */
+
+#include "RV32IMACFDV_zvl64bArch.h"
+
+#define ETISS_ARCH_STATIC_FN_ONLY
+#include "RV32IMACFDV_zvl64bFuncs.h"
+
+using namespace etiss;
+using namespace etiss::instr;
