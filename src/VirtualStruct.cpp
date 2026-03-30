968be532b30b7d9bf62e5cc0da809165f77ffd4f
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-03-30 15:26:07 +0200

    src/VirtualStruct.cpp: set default offset of read/write to 0

--- src/VirtualStruct.cpp
+++ src/VirtualStruct.cpp
@@ -43,7 +43,7 @@ VirtualStruct::Field::Field(VirtualStruct &parent, const std::string &name, cons
 
 VirtualStruct::Field::~Field() {}
 
-uint64_t VirtualStruct::Field::read(size_t offset) const
+uint64_t VirtualStruct::Field::read(size_t offset = 0) const
 {
 
     if (!(flags_ & R))
@@ -79,7 +79,7 @@ uint64_t VirtualStruct::Field::read(size_t offset) const
 
     return ret;
 }
-void VirtualStruct::Field::write(uint64_t val, size_t offset)
+void VirtualStruct::Field::write(uint64_t val, size_t offset = 0)
 {
 
     if (!(flags_ & W))
@@ -122,7 +122,7 @@ void VirtualStruct::Field::signalWrite()
     {
         if (l.first)
         {
-            l.first->write(*this, read(0));
+            l.first->write(*this, read());
         }
     }
 }
@@ -169,10 +169,10 @@ bool VirtualStruct::Field::_applyBitflip(unsigned position, uint64_t fault_id)
         return lapplyBitflip(position, fault_id);
     if ((flags_ & RW) != RW)
         return false;
-    uint64_t val = read(0);
+    uint64_t val = read();
     uint64_t errval = ((uint64_t)1) << position;
     errval = val ^ errval;
-    write(errval, 0);
+    write(errval);
     std::stringstream ss;
     ss << "Injected bitflip in " << name_ << " 0x" << std::hex << val << "->0x" << errval << std::dec;
     etiss::log(etiss::INFO, ss.str());
@@ -184,7 +184,7 @@ bool VirtualStruct::Field::_applyAction(const etiss::fault::Fault &f, const etis
     if (a.getType() == +etiss::fault::Action::type_t::MASK)
     {
         uint64_t mask_value = a.getMaskValue();
-        uint64_t val = read(0), errval;
+        uint64_t val = read(), errval;
         switch (a.getMaskOp())
         {
         case etiss::fault::Action::mask_op_t::AND:
@@ -206,7 +206,7 @@ bool VirtualStruct::Field::_applyAction(const etiss::fault::Fault &f, const etis
             errval = val;
             break;
         }
-        write(errval, 0);
+        write(errval);
         std::stringstream ss;
         ss << "Injected mask fault in " << name_ << " 0x" << std::hex << val << " " << a.getMaskOp() << " 0x"
            << mask_value << "->0x" << errval << std::dec;
@@ -463,7 +463,7 @@ bool VirtualStruct::readField(void *fastfieldaccessptr, uint64_t &val, std::stri
         errormsg = "No read access";
         return false;
     }
-    val = f->read(0);
+    val = f->read();
     return true;
 }
 bool VirtualStruct::applyAction(const etiss::fault::Fault &fault, const etiss::fault::Action &action,
@@ -620,7 +620,7 @@ void copy(VirtualStruct &dst, VirtualStruct &src, std::list<std::shared_ptr<Virt
             }
 
             if (!pretend)
-                dstf->write(srcf->read(0), 0); // copy value
+                dstf->write(srcf->read()); // copy value
         });
 
     dst.foreachField(
