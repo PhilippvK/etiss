7a9d33832b5caf4d9a2d3517c1d7a3fb16176be1
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-03-30 15:43:26 +0200

    RV32IMACFDV: re-add dropped VectorRegField_RV32IMACFDV

--- ArchImpl/RV32IMACFDV/RV32IMACFDVArchSpecificImp.h
+++ ArchImpl/RV32IMACFDV/RV32IMACFDVArchSpecificImp.h
@@ -122,6 +122,48 @@ class FloatRegField_RV32IMACFDV : public etiss::VirtualStruct::Field
     }
   };
 
+class VectorRegField_RV32IMACFDV : public etiss::VirtualStruct::Field{
+private:
+       const unsigned gprid_;
+public:
+       VectorRegField_RV32IMACFDV(etiss::VirtualStruct & parent,unsigned gprid)
+               : Field(parent,
+                       std::string("V")+etiss::toString(gprid),
+                       std::string("V")+etiss::toString(gprid),
+                       R|W,
+                       // 1
+                       8}  // ?
+               ),
+               gprid_(gprid)
+       {}
+
+       VectorRegField_RV32IMACFDV(etiss::VirtualStruct & parent, std::string name, unsigned gprid)
+               : Field(parent,
+                       name,
+                       name,
+                       R|W,
+                       // 1
+                       8}  // ?
+               ),
+               gprid_(gprid)
+       {}
+
+       virtual ~VectorRegField_RV32IMACFDV(){}
+
+protected:
+       virtual uint64_t _read(size_t offset) const {
+               // printf("v_read with gprid_ %d\n", gprid_);
+               // TODO: check for out of bounds? (offset < width_/8)
+               return (uint64_t) *((uint64_t*)&((RV32IMACFDV*)parent_.structure_)->V[gprid_ * width_ + sizeof(uint64_t) * offset]);
+       }
+
+       virtual void _write(uint64_t val, size_t offset) {
+               etiss::log(etiss::VERBOSE, "write to ETISS cpu state", name_, val);
+               // printf("v_write (%lu) with gprid_ %d\n", val, gprid_);
+               // TODO: check for out of bounds? (offset < width_/8)
+               *((uint64_t*)&((RV32IMACFDV*)parent_.structure_)->V[gprid_ * width_ + sizeof(uint64_t)]) = (etiss_uint64) val;  // TODO: write V[gprid_] instead
+       }
+};
 
 class CSRField_RV32IMACFDV : public etiss::VirtualStruct::Field
 {
